5fcc4513f5834567c06e22ffdec9d834c6bda114
Alain SIANI <asiani@free.fr>
2023-05-27 00:20:51 +0200

    CREATE : Project model

--- softdesk/issue_tracking_system/models.py
+++ softdesk/issue_tracking_system/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# 
+# Initialization : Project model
+#
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Projects"
+
+    def __str__(self):
+        return self.title
+
