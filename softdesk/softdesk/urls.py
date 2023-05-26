a9da0b30fa14f1a131b1a3ec4e1ad7cb78b7b678
Alain SIANI <asiani@free.fr>
2023-05-26 19:19:45 +0200

    CREATE : First view

--- softdesk/softdesk/urls.py
+++ softdesk/softdesk/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
+
+    # Administration views
     path("admin/", admin.site.urls),
+
+    # My first view
+    path("", views.index, name="index"),
+
+
 ]
