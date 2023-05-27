5a3df57d05bd9ae578158e8fc3f20eec065230c9
Alain SIANI <asiani@free.fr>
2023-05-27 12:03:41 +0200

    SET : Register Contributor in admin site

--- softdesk/issue_tracking_system/admin.py
+++ softdesk/issue_tracking_system/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 from .models import Project
+from .models import Contributor
+
 
 # Register your models here.
 
 admin.site.register(Project)
+admin.site.register(Contributor)
