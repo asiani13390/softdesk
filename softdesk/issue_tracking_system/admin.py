ba44395cb70ddebcedb8968c1bbce979ad46b566
Alain SIANI <asiani@free.fr>
2023-05-27 12:12:43 +0200

    SET : Register Comment in admin site

--- softdesk/issue_tracking_system/admin.py
+++ softdesk/issue_tracking_system/admin.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 from .models import Project
 from .models import Contributor
 from .models import Issue
+from .models import Comment
 
 # Register your models here.
 
 admin.site.register(Project)
 admin.site.register(Contributor)
 admin.site.register(Issue)
+admin.site.register(Comment)
+
